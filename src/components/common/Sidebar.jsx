e6f7583e771a33407e8c343cc3f179ce3de3a684
Hisham Isah Dona <hishamisahdona@gmail.com>
2026-08-25 22:03:52 +0100

    fix: restore React and useLocation imports in Sidebar.jsx

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
 import { authService } from '../../services/authService';
 import FudLogo from './FudLogo';
 import { 
